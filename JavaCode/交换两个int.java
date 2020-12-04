6b0ec48e155d8119201dfd40f953994acc4ab6e8
LiKunlong <lkl0801@sina.cn>
2020-12-04 09:38:20 +0800

    提交代码

---
+++ JavaCode/交换两个int.java
@@ -0,0 +1,18 @@
+ public static void main(String[] args) {
+      //  System.out.println("hello,world!");
+        //交换两个int
+        int a=10;
+        int b=20;
+        //①创建第三个变量
+       /* int c=a;
+        a=b;
+        b=c;*/
+        //②加减法
+        /*a=a+b;
+        b=a-b;
+        a=a-b;*/
+        //③运算符
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("a="+a+",b="+b);
