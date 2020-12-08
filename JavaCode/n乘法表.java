74b25c63480411810985e22b51bcc585d96c334f
LiKunlong <lkl0801@sina.cn>
2020-12-08 10:06:10 +0800

    提交代码

---
+++ JavaCode/n乘法表.java
@@ -0,0 +1,11 @@
+//输出乘法口诀表(n由用户输入)
+    public static void main(String[] args) {
+        System.out.println("输入n决定几行几列:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                System.out.printf("%d*%d=%d\t",i,j,i*j);
+            }
+        }
+    }
