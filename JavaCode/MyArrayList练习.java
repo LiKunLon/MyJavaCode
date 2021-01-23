20a6b72f9c62afa3eddd4d0b8bfa134d63baed9a
LiKunlong <lkl0801@sina.cn>
2021-01-23 12:28:28 +0800

    提交代码

---
+++ JavaCode/MyArrayList练习.java
@@ -0,0 +1,40 @@
+/*class MyArrayList<E>{
+    public E[] elem;
+    public int usedSize;
+    public MyArrayList(){
+        this.elem=(E[])new Object[10];
+    }
+
+    public void add(E data){
+        this.elem[usedSize]=data;
+        this.usedSize++;
+    }
+    public E get(int pos){
+        return this.elem[pos];
+    }
+
+
+}*/
+
+   /* public static void main(String[] args) {
+        MyArrayList<String> myArrayList=new MyArrayList<>();
+        myArrayList.add("abc");
+        myArrayList.add("def");
+        myArrayList.add("gh");
+        String ret=myArrayList.get(0);
+        System.out.println(ret);
+
+        MyArrayList<Integer> myArrayList1=new MyArrayList<>();
+        myArrayList1.add(1);
+        myArrayList1.add(112);
+        myArrayList1.add(12903);
+        int num=myArrayList1.get(2);
+        System.out.println(num);
+
+        MyArrayList<Double> myArrayList2=new MyArrayList<>();
+        myArrayList2.add(1.0);
+        myArrayList2.add(5.6);
+        myArrayList2.add(4.3);
+        double val=myArrayList2.get(1);
+        System.out.println(val);
+    }*/
