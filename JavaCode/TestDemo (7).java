eaf6e3452b09f391eb4728979244dff3f86fceb6
LiKunlong <lkl0801@sina.cn>
2021-02-07 19:36:59 +0800

    提交代码

---
+++ JavaCode/TestDemo (7).java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: GaoBo
+ * Date: 2021-02-05
+ * Time: 8:42
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
