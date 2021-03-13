6aa2b58ae9944975140799523114193289f062b3
LiKunlong <lkl0801@sina.cn>
2021-03-13 15:23:05 +0800

    力扣题

---
+++ JavaCode/宝石与石头(力扣).java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        int jlen=jewels.length();
+        int slen=stones.length();
+        for(int i=0;i<jlen;i++){
+            char a=jewels.charAt(i);
+            for(int j=0;j<slen;j++){
+                char b=stones.charAt(j);
+                if(a==b){
+                    count++;
+                    
+                    //找到一个,计数器加一,开始下一个jewel
+                }
+            }
+        }
+        return count;
+    }
+}
