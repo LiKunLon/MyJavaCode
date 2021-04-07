8e3c17603e010e4940a365886b5be3345e755f78
LiKunlong <lkl0801@sina.cn>
2021-04-08 07:47:45 +0800

    提交

---
+++ JavaCode/就地删除元素(牛客).java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        int j=0;
+        for(int i=0;i<A.length;++i){
+            if(A[i]!=elem){
+                A[j]=A[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
