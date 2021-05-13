e4dc43efcf634d04ed0663cfec1c15334f5eb73d
LiKunlong <lkl0801@sina.cn>
2021-05-13 17:28:02 +0800

    提交

--- JavaCode/从尾到头打印链表.java
+++ JavaCode/从尾到头打印链表.java
@@ -1,25 +1,53 @@
+/**
+*    public class ListNode {
+*        int val;
+*        ListNode next = null;
+*
+*        ListNode(int val) {
+*            this.val = val;
+*        }
+*    }
+*
+*/
 import java.util.ArrayList;
 public class Solution {
+   //  ArrayList<Integer> list=new ArrayList<>(); //2
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        ArrayList<Integer>  list=new ArrayList<>();
-        if(listNode==null)return list;
-        ListNode newHead=null;
-        ListNode cur=listNode;
-        ListNode prev=null;
-        while(cur!=null){
-            ListNode curNext=cur.next;
-            if(curNext==null){
-                newHead=cur;
-            }
-            cur.next=prev;
-            prev=cur;
-            cur=curNext;
-        }
+//         ArrayList<Integer>  list=new ArrayList<>();
+//         if(listNode==null)return list;
+//         ListNode newHead=null;
+//         ListNode cur=listNode;
+//         ListNode prev=null;
+//         while(cur!=null){
+//             ListNode curNext=cur.next;
+//             if(curNext==null){
+//                 newHead=cur;
+//             }
+//             cur.next=prev;
+//             prev=cur;
+//             cur=curNext;
+//         }
         
-        while(newHead!=null){
-            list.add(newHead.val);
-            newHead=newHead.next;
-        }
-        return list;
+//         while(newHead!=null){
+//             list.add(newHead.val);
+//             newHead=newHead.next;
+//         }
+        
+        
+        //2
+//         if(listNode!=null){
+//             list.add(0,listNode.val);
+//             printListFromTailToHead(listNode.next);
+//         }
+//         return list;
+          
+        //3
+//         ArrayList<Integer> list=new ArrayList<>();
+//         ListNode cur=listNode;
+//         while(cur!=null){
+//             list.add(0,cur.val);
+//             cur=cur.next;
+//         }
+//         return list;
     }
 }
