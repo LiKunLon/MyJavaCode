41c01d8db010a9f2ff93d3104cc4af7004b58442
LiKunlong <lkl0801@sina.cn>
2021-06-05 15:09:02 +0800

    提交

---
+++ JavaCode/合并K个升序链表(力扣).java
@@ -0,0 +1,35 @@
+纪念一下力扣上完成的第一个困难题
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode res=null;
+        for(int i=0;i<lists.length;i++){
+            res=merge(res,lists[i]);
+        }
+        return res;
+    }
+    public ListNode merge(ListNode a,ListNode b){
+        if(a==null)return b;
+        if(b==null)return a;
+        ListNode newHead=new ListNode(-1);
+        ListNode cur=newHead;
+        ListNode list1=a;
+        ListNode list2=b;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                cur.next=list1;
+                list1=list1.next;
+                cur=cur.next;
+            }else{
+                cur.next=list2;
+                list2=list2.next;
+                cur=cur.next;
+            }
+        }
+        if(list1!=null){
+            cur.next=list1;
+        }else{
+            cur.next=list2;
+        }
+        return newHead.next;
+    }
+}
